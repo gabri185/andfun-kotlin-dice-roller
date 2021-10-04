d3b758cea74fb870fd4b7078bb27a31cdd336686
gabri185 <gdominguezreyes@danielcastelao.org>
2021-10-04 08:48:02 +0200

    Tercer paso del proyecto hecho

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -18,8 +18,11 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,9 +30,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+            //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         }
     }
-}
+
+        fun rollDice (){
+            val randomInt = Random().nextInt(6)+1
+
+            val ResultText : TextView = findViewById(R.id.result_text)
+            ResultText.text = randomInt.toString()
+
+        }
+
+    }
